d831131f831c14fb5aebf3280c293ca2cc0e59fa
KyletheFox <ko163@me.com>
2016-01-31 01:32:46 -0500

    Added descriptions and comments

--- ReadWrite.c
+++ ReadWrite.c
@@ -1,3 +1,54 @@
+/*
+
+ReadWrite.c
+@author Kyle O'Neill
+
+The main purpose of this program is to read and write records to a text file inside the 
+the directory. The secondary function of this program is to write a timestamp to a file
+of when this program starts.
+
+To appoarch this problem I divided this program into two parts. The first part was to 
+read and write records into a file and to write a time stamp to another file.
+
+For the first part of my program, I analysed the problem and decide to divide it into three parts.
+The three parts of my program include a record generatior, a record reader, and a controler. 
+These part help naturally divided the program into the three tasks I need
+to meet one of the requirements of this lab.
+
+The first part of the programm is the recordGen(). This function takes two parameters
+a file pointer and an array of strings. The file pointer is the file which the reorcdGenerator()
+is to write the records to and the array of strings is an array of all the records that are
+written to the file. The function uses a nested for loop to generate the records. The inside 
+loop, loops 120 times, placing a randonly generated chartacter into a temporary string. After
+this loop is finished, that string has the EOF charter added, written to the array of strings
+passed into the function and then written to the file. The outside loop repeates all of this to 
+generate 10 seperate records inside the file pointer passed into the function.
+
+The output of recordGen is the 10 records written to the file pointer that was passed in as 
+an argument. The records contain 120 randonmly generated characters with ASCII values 65 - 122
+plus one EOF charater. These records can contain any of the different possible combinations
+of these particular characters.
+
+The second part of the code is the searchFile(). This function has two parameters passed to it.
+The two parameters are a FILE pointer and a string which contains one of the randomly generated
+records that recordGen() created. This function then uses a for loop to linearly search the 
+file for a record that matches the randomly generated one passed to the function. Inside the loop, 
+I scaned the first record from the file and compared it to record that was passed into the function.
+This loop will continue until a match is found or the loop has read all the records in the file. 
+If the record was not found, a message would be print to the screen.
+
+The third part of the program is the controller which is the main() in this program. The main 
+calls the functions, opens and rewinds the file pointer and generates a random record for
+recordGen() to search for. 
+
+The other purpose of this code is to create a time stamp of when this program starts. To 
+accomplish this, I used the gettimeofday function from the <sys/time.h> library. I used this
+function to return a timeval struct, which is also apart of that same library. One of the 
+fields of this struct holds a timestamp in mircoseconds. A file is opened up and thios time 
+stamp is writen to the file. 
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,25 +61,30 @@ void searchFile(FILE *fp, char *record);
 int main(int argc, char const *argv[])
 {	
 	// ---- Get End Time ---------
-	struct timeval end;
-	gettimeofday(&end, NULL);
-	FILE *fp;
+	struct timeval end;			// Timestamp Struct
+	FILE *fp;					// File pointer for timestamp output
 
+	// Gets the timestamp
+	gettimeofday(&end, NULL);
+
+	// Open file to output timestamp too
 	if ((fp=fopen(argv[1], "a"))== NULL) {
 		fprintf(stderr, "ERROR opening %s\n", argv[1]);
 	}
 	
-	fprintf(fp, "%d\n", end.tv_usec);
+	// Print timestamp to file
+	fprintf(fp, "%d\n", end.tv_usec); 
 	fclose(fp);
 
 	// --------------------------------
 
+	// ------ Variables ----------------
+	int randRecNum;				// Placeholder for rand number
+	char records[10][121]; 		// Array of Strings to hold all the records generated
+	char *randRec;				// Placeholder for the randomly generated record
+	FILE *fs;					// File pointer to the text file where the records are writen.
 
-	int randRecNum;
-	char records[10][121]; 
-	char *randRec;
-	FILE *fs;
-
+	// Open file to put write/read records from
 	if ((fs=fopen("Records.txt", "w+"))== NULL) {
 		fprintf(stderr, "ERROR opening %s\n", argv[1]);
 	}
@@ -42,9 +98,6 @@ int main(int argc, char const *argv[])
 	// Pick a random record (0-9)
 	randRecNum = rand() % 10;
 	randRec = records[randRecNum];
-
-	// printf("Record Number: %d\n", randRecNum);
-	//printf("Record to search for: %s\n", randRec);
 	
 	// Rewind File
 	rewind(fs);
@@ -52,14 +105,22 @@ int main(int argc, char const *argv[])
 	// Read File and Search for Record
 	searchFile(fs, randRec);
 
-	remove("Records.txt");
+	remove("Records.txt"); 		// Deletes file as per spec
 	fclose(fs);
 	return 0;
 }
 
+/* 
+This function generates and writes records to file and array that 
+is passed in as arguments.
+*/
 void recordGen(FILE *fp, char records[10][121]) {
-	char string[121];
-	int i, j;
+	// --------- Input -----------
+	//	*fp - File to write records too
+	//  records - Array to hold all the records that are generated
+
+	char string[121];	// Temporary place holder for generated records
+	int i, j;			// Loop Counters
 
 	for (j = 0; j < 10; j++) {
 	
@@ -76,6 +137,7 @@ void recordGen(FILE *fp, char records[10][121]) {
 	string[120] = '\0';
 	records[j][120] = '\0';
 
+	// Writes record and new line character to file
 	fputs(string, fp);
 	fputc('\n', fp);
 
@@ -83,16 +145,27 @@ void recordGen(FILE *fp, char records[10][121]) {
 
 }
 
+/*	
+This function searches the file that records were writen too and compares
+them to a randomly generated record.
+*/
 void searchFile(FILE *fp, char *record) {
-	char found = 0;
-	char str[121];
+	// --------------- Input -----------
+	//	*fp - file that contains the records
+	//	*record - a randonly selected record that was created by recordGen()
 
+	char found = 0;		// Flag to break loop if found
+	char str[121];		// Temporary placeholder for strings that are read from file
+
+
+	// Loop to search through file - Linear Search
 	while (fgets(str, 121, fp) && !found) {
 		if (strcmp(str, record) == 0) {
 			found = 1;
 		}
 	}
 
+	// Print out statement if no matching record was found
 	if (str == (char *)EOF)	{
 		printf("Record Not Found.\n");
 	}
--- Timer.c
+++ Timer.c
@@ -1,3 +1,50 @@
+/*
+
+Timer.c
+@author Kyle O'Neill
+
+The purpose of this program is to measure the time it takes for an application to 
+start. These times will come from the launching of ./write which is the binary that
+is created from the ReadWrite.c program. This program needs to be run for several 
+minutes so there are 100,000 executions of this program which allows us to analyze the
+times required to start this particular program.
+
+To tackle this problem the first thing I needed was to generate files for the timestamps
+to be writen to for later analyze. The best way to do this was to put each time into the
+file on a new line. I decided to have a total of 4 different files to collect all the times.
+Two of the files would contain the start times before the process starts. One for the first
+run of the application and another for the second one. The two other files would hold the 
+results but this files are created inside the ReadWrite by being passed as an argument. 
+These files would hold the time stamps after the program starts.
+
+The second part of the problem was launching the applications themselves. To do this
+I used a nested conditional statement inside a for loop. First the program got the timestamp
+of the program and printed it to the startTime1.txt. Then the program calls the fork method
+which copies the current process. The program then checks to see if itself is a fork or not. 
+If the program is a fork then it runs the program using the execl function. Inside this
+function I also pass resultTime1.txt to record the program starts time.
+Else, the program gets another time stamp and places it into startTime2.txt. The program then
+calls the fork() function again and checks to see if the program is a child or not. If it is,
+it uses execl with the parameter file name resultTime2.txt. This entire process happens in a 
+loop. This loop is set to run for 50000 runs which runs the application 100000 times.
+
+This process is shortened when the program needs only to run the program once per loop. To 
+Create the data for that test, the internal conditional statement is commented out, the 
+loop is set to run for 100000 times and the program is then recomplied. This will give us 
+the data only using one fork.
+
+Another important part of this program is the wait() function call. These calls that are 
+inside the loop make the parent process wait for the child process to finish. This is 
+needed in my program because there would be times where two of the same children will be 
+running at the same time which cause the result times to become corrupted. The wait() function
+calls solve this program.
+
+I also opened and closed the resultTime1.txt and resultTime2.txt files in write mode to
+clear the files if there was any previous data left in them. The ReadWrite program only 
+appends to the file so the old data needs to be cleaned before they can be used again.  
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
@@ -13,10 +60,10 @@ int main(int argc, char const *argv[])
 	// -------------------------------------------------
 
 
-	// Variables
-    struct timeval sp;
-    FILE *fs, *fe;
-    int i;
+	// ----------- Variables ------------------
+    struct timeval sp;		// Timestamp struct (from <sys/time.h>)
+    FILE *fs, *fe;			// File Pointers to StartTime text files
+    int i;					// Loop counter
   
 
     // Open Files
@@ -30,30 +77,34 @@ int main(int argc, char const *argv[])
 	
 	// Loop through forks
 	for (i = 0; i < 50000; ++i)	{
-		
+	
+	// Get and print time stamp
 	gettimeofday(&sp, NULL);
 	fprintf(fs, "%d\n", sp.tv_usec);
 
-		if (!fork()) {
+		// Fisrt Fork call (!fork() == child)
+		if (!fork()) {	
 			execl("./write", "./write", "resultTime1.txt", (char *)0);
 		}
 		else {
 			
+			// First wait 
 			wait(NULL);
+			
+			// Get and print second time stamp
 			gettimeofday(&sp, NULL);
 			fprintf(fe, "%d\n", sp.tv_usec);
 
+			// Second Fork
 			if (!fork())	{
 				execl("./write", "./write", "resultTime2.txt", (char *)0);
 			}
 		}
-		wait(NULL);
 
-		if ((i%500) == 0) {
-			printf("#");
-		}
+		wait(NULL);
 	}
 
+	// Print out program is finished to user
 	printf("\nFinished!\n");
 	fclose(fs);
 	fclose(fe);
